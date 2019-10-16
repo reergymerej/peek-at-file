d74aede3169314285b51485e299fe674ad7ae9dd
Jeremy Greer <jex.grizzle@gmail.com>
2019-10-16 09:34:52 -0700

    Return string version of binary

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # peek-at-file
+
+How can we send binary using curl?
+https://stackoverflow.com/questions/4053937/post-binary-file-with-cmd-line-curl-using-headers-contained-in-the-file
+
+```
+curl \
+       --header "Content-Type:application/octet-stream" \
+       --data-binary @some.file \
+       http://localhost:3333
+```
+
+How can I receive the file in Node?
+  Read data streamed in the request, on 'data'.
--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@ import {
 } from 'http'
 
 const server = createServer((req, res) => {
-  console.log('hello')
-  res.end()
+  // Read the file.
+  // https://stackoverflow.com/questions/4053937/post-binary-file-with-cmd-line-curl-using-headers-contained-in-the-file
+  const chunks = []
+  req.on('data', (chunk) => {
+    chunks.push(chunk)
+  })
+    .on('end', () => {
+      const buffer = Buffer.concat(chunks)
+      const string = buffer.toString('utf8')
+      console.log(`received ${chunks.length} chunks`)
+      // console.log(string)
+      res.write(string)
+      res.end()
+    })
 })
 
 const port = 3333
